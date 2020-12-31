8d98a7ff2cec7cd5797eec0fc4d221538f7b2cf4
minyo <005snail@gmail.com>
2020-12-31 18:59:47 +0900

    history

---
+++ src/Profile.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const data = {
+  velopert: {
+    name: "김민준",
+    desc: "리액트를 좋아하는 개발자"
+  },
+  jihoon: {
+    name: "박지훈",
+    desc: "맥깅이를 사랑하는 윙깅이"
+  }
+};
+
+const Profile = ({ match }) => {
+  const { username } = match.params;
+  const profile = data[username];
+  if (!profile) {
+    return <div>존재하지 않는 사용자입니다.</div>;
+  }
+  return (
+    <div>
+      <h3>
+        {username}({profile.name})
+      </h3>
+      <p>{profile.desc}</p>
+    </div>
+  );
+};
+
+export default Profile;
